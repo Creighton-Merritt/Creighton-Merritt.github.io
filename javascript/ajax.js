95edb7eda053e15bbe01139631b3c471f18f8c84
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-11-12 15:24:53 -0700

    Ajax

--- javascript/ajax.js
+++ javascript/ajax.js
@@ -1,14 +1,16 @@
 function getJson() {
     var xmlhttp = new XMLHttpRequest();
     var results = document.getElementById('display');
-
+    var i = 0;
+    
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var data = JSON.parse(xmlhttp.responseText);
-            for (i = 0; i < data.length; i++) {
-                results.innerHTML += '<li>' + data[i] + '</li>';
-                console.log(data);
+            do {
+                results.innerHTML += data.fruits[i] + '<br>';
+                i++;
             }
+            while (i < 9)
         }
     };
 
