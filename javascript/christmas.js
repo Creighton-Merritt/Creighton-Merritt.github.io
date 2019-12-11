95b4dfae5d2cb33594496ab316f8d5200ebed33c
Merritt C <35177108+Merritt-Jean@users.noreply.github.com>
2019-12-11 13:21:59 -0700

    Added listeners for button clicks

--- javascript/christmas.js
+++ javascript/christmas.js
@@ -1,36 +1,41 @@
-
-class Items {
-    constructor(item) {
-        this.item = item;
-    }
-};
-
-//Load the current list on page load
-function display() {    
+// Load the current list in local storage
+window.onload = function() {
     localArr = JSON.parse(localStorage.getItem('localArr')) || [];
     for(var i=0; i < localArr.length; i++) {
-        tbody = document.getElementById("tableBody");
-        row = tbody.insertRow();
-        cell1 = row.insertCell();
-        cell1.innerHTML = localArr[i];
+        var markup = "<tr><td><input type='radio' id='radio' name='record' value='"+localArr[i]+"'>"+localArr[i]+"</td></tr>";
+        $("#tableBody").append(markup);
+        $
     }
 };
 
-//Add item to storage and add to the list table
-function addToList() {
-    var itemName = document.getElementById("itemName").value;
 
-    if(itemName != "") {
-        localArr = JSON.parse(localStorage.getItem('localArr')) || [];
-        localArr.push(itemName);
-        localStorage.setItem("localArr", JSON.stringify(localArr));
-
-        tbody = document.getElementById("tableBody");
-        row = tbody.insertRow();
-        cell1 = row.insertCell();
-        cell1.innerHTML = itemName;   
-
+// Add to local storage and add to the list on screen
+$(document).ready(function(){
+    $(".add-row").click(function(){
+        var itemName = $("#itemName").val();
+        if(itemName != "") {
+            localArr = JSON.parse(localStorage.getItem('localArr')) || [];
+            localArr.push(itemName);
+            localStorage.setItem("localArr", JSON.stringify(localArr));
+            var markup = "<tr><td><input type='radio' id='radio' name='record' value='"+itemName+"'>"+itemName+"</td></tr>";
+            $("#tableBody").append(markup);
+        }
         document.getElementById("itemName").value = "";
-    }
-    console.log(localArr.length);
-};
+    });
+
+  //  Delete all checked rows and remove them from local storage using 'splice'
+    $(".delete-row").click(function(){
+        $("#tableBody").find('input[name="record"]').each(function(i){
+            if($(this).is(":checked")){
+                var $selected = $(this).val();
+                $(this).parents("tr").remove();
+                localArr = JSON.parse(localStorage.getItem('localArr')) || [];
+                if($selected == localArr[i]) {
+                    localArr.splice(i,1);
+                    localStorage.setItem("localArr", JSON.stringify(localArr));
+                    return false;
+                }
+            }
+        });
+    });
+});
